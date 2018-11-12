b89003ebe23ac87635bbfa97ee5c2304eabe1769
Alexis Lopez <alexislopezg07@gmail.com>
2018-11-12 16:42:45 -0400

    Add second test case

--- tests/test_firsttestcase.py
+++ tests/test_firsttestcase.py
@@ -1,20 +1,26 @@
 # content of test_firsttestcase.py
-import pytest
-from chromedriver_binary.utils import get_chromedriver_path
-from pytruth.truth import truth
-from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
+
+from pytruth.truth.truth import AssertThat
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-@pytest.fixture
-def setup():
-    ChromeDriver(get_chromedriver_path())
+def test_is_user_in_website(browser):
+    AssertThat(browser.current_url).IsEqualTo("https://dev.to/")
 
 
-def test_is_user_in_website(driver):
-    driver.get("https://dev.to/")
-    truth.AssertThat(driver.current_url).IsEqualTo("https://dev.to/")
-
-
-@pytest.fixture
-def teardown(self):
-    self.driver.close()
+def test_can_user_search_for_posts(browser):
+    search_bar = browser.find_element_by_xpath("//input[@placeholder='search']")
+    search_bar.send_keys("must have extensions")
+    search_bar.send_keys(Keys.ENTER)
+    WebDriverWait(browser, 60).until(expected_conditions.url_contains("https://dev.to/search?q="))
+    WebDriverWait(browser, 60).until(expected_conditions.presence_of_element_located((
+        By.XPATH, "//div[contains(@class, 'single-article')][1]//h3")))
+    post = browser.find_element_by_xpath("//div[contains(@class, 'single-article')][1]//h3")
+    post.click()
+    WebDriverWait(browser, 60).until(
+        expected_conditions.url_to_be("https://dev.to/chrisvasqm/must-have-extensions-for-vs-code-according-to-me-dho"))
+    AssertThat(browser.current_url).IsEqualTo(
+        "https://dev.to/chrisvasqm/must-have-extensions-for-vs-code-according-to-me-dho")
