4a0f7c7efd32e2590e33be52a6c6c55107b8f260
Alexis Lopez <alexislopezg07@gmail.com>
2018-11-14 11:12:52 -0400

    Refactor code

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,34 @@ from selenium import webdriver
 import chromedriver_binary
 from selenium.webdriver.support.wait import WebDriverWait
 
+from Page.HomePage import HomePage
+from Page.PostPage import PostPage
+from Page.SearchPage import SearchPage
 
-@pytest.fixture()
+
+@pytest.fixture(autouse=True)
 def browser():
     with webdriver.Chrome() as browser:
         print("Creating a Chrome Browser Instance")
+        homepage = HomePage(browser)
+        searchpage = SearchPage(browser)
+        postpage = PostPage(browser)
         browser.maximize_window()
         browser.get("https://dev.to/")
         print("Opening " + browser.current_url)
         yield browser
+
+
+@pytest.fixture()
+def homepage(browser):
+    return HomePage(browser)
+
+
+@pytest.fixture()
+def searchpage(browser):
+    return SearchPage(browser)
+
+
+@pytest.fixture()
+def postpage(browser):
+    return PostPage(browser)
--- tests/test_firsttestcase.py
+++ tests/test_firsttestcase.py
@@ -2,19 +2,12 @@
 
 from pytruth.truth.truth import AssertThat
 
-from Page.HomePage import HomePage
-from Page.PostPage import PostPage
-from Page.SearchPage import SearchPage
 
-
-def test_is_user_in_website(browser):
+def is_user_in_website(browser):
     AssertThat(browser.current_url).IsEqualTo("https://dev.to/")
 
 
-def test_can_user_search_for_posts(browser):
-    homepage = HomePage(browser)
+def test_can_user_search_for_posts(homepage, searchpage, postpage):
     homepage.search_for("must have extensions")
-    searchpage = SearchPage(browser)
     searchpage.select_first_result()
-    postpage = PostPage(browser)
     AssertThat(postpage.is_readable()).IsTrue()
