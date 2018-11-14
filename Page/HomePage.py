57e78785e6eafc4f2d00495606a75b4a48b2f103
Alexis Lopez <alexislopezg07@gmail.com>
2018-11-14 11:35:40 -0400

    Refactor code

--- Page/HomePage.py
+++ Page/HomePage.py
@@ -11,6 +11,12 @@ class HomePage(BasePage):
         super().__init__(browser)
         self.browser = browser
 
+    def is_visible(self):
+        if self.browser.current_url == "https://dev.to/":
+            return True
+        else:
+            return False
+
     def search_for(self, query):
         self.browser.find_element(By.XPATH, "//input[@placeholder='search']").send_keys(query, Keys.ENTER)
         Wait(self.browser, self.timeout).until(expected_conditions.url_contains("https://dev.to/search?q="))
--- tests/test_firsttestcase.py
+++ tests/test_firsttestcase.py
@@ -3,8 +3,8 @@
 from pytruth.truth.truth import AssertThat
 
 
-def is_user_in_website(browser):
-    AssertThat(browser.current_url).IsEqualTo("https://dev.to/")
+def test_is_user_in_website(homepage):
+    AssertThat(homepage.is_visible()).IsTrue()
 
 
 def test_can_user_search_for_posts(homepage, searchpage, postpage):
